9a8c6fceee2d973641e4a1f7234956fb86b490f6
Mathieu Suchet <msuchet63@gmail.com>
2026-04-04 20:33:56 +0200

    Add an upper bound for name length

--- src/components/AddProject.tsx
+++ src/components/AddProject.tsx
@@ -15,15 +15,15 @@ function AddProject({addProject}) {
     // Name validation
     const name: string | undefined = formData.get("projectName");
 
-    if(name === undefined || name.trim().length == 0){
-      setNameError("Name needs to have a value");
+    if(name === undefined || name.trim().length == 0 || name.trim().length > 25){
+      setNameError("Name needs to have a value or be shorter than 25 characters");
       return;
     }
     
 
     setNameError("");
 
-    createProject(name)
+    createProject(name.trim())
   }
 
   if(creating){
@@ -33,19 +33,20 @@ function AddProject({addProject}) {
         <form action={create}>
           <div className="form-group">
           <label htmlFor="pName">Project name</label>
-          <input type="text" name="projectName" className="form-control" id="pName" aria-describedby="pName-help"/>
-          <small id="pName-help" className="form-text text-muted">The name for your project (does not need to be unique)</small>
+          <input type="text" name="projectName" className="form-control" id="pName" aria-describedby="pName-help" placeholder="My best project"/>
+          <small id="pName-help" className="form-text text-muted">The name for your project</small>
           <p className="form-text text-danger">{nameError}</p>
         </div>
 
         <button type="submit" className="btn btn-primary">Create project</button>
+        <button className="btn btn-secondary" onClick={() => setCreating(false)}>Cancel</button>
         </form>
         </div>
     )
   }
   else{
     return (
-      <button type="button" className="btn btn-primary" onClick={() => setCreating(!creating)}>Add project</button>
+      <button type="button" className="btn btn-primary" onClick={() => setCreating(true)}>Add project</button>
     )
   }
 }
