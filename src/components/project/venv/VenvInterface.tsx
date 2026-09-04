4c9433359a98a5250e5931614b69f867b915ed15
Mathieu Suchet <msuchet63@gmail.com>
2026-09-04 18:14:05 +0200

    Remove getting default python on load

--- src/components/project/venv/VenvInterface.tsx
+++ src/components/project/venv/VenvInterface.tsx
@@ -18,7 +18,6 @@ function VenvInterface({projectMetadata, updateProjectExecutable}: VenvInterface
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
     const handleCreateClose = () => setIsCreateModalOpen(false);
-    const handleCreateShow = () => setIsCreateModalOpen(true);
 
     const handlePackageClose = () => setIsPackageModalOpen(false);
     const handlePackageShow = () => setIsPackageModalOpen(true);
@@ -27,6 +26,8 @@ function VenvInterface({projectMetadata, updateProjectExecutable}: VenvInterface
     const handleDeleteShow = () => setIsDeleteModalOpen(true);
 
     const {createVenv, deleteVenv, getPackages, packages, installPackage, installRequirements, uninstall, packagesToUpdate, getPackagesToUpdate, update, getPythonDefaultExecutables, pythonDefaults} = useVenv({metadata: projectMetadata, updatePythonInterpreter: updateProjectExecutable})
+
+    const handleCreateShow = () => {getPythonDefaultExecutables().then(() => setIsCreateModalOpen(true))};
 
     const createVenvModal = (python_executable: string) => {
         createVenv(python_executable).then(
@@ -68,13 +69,42 @@ function VenvInterface({projectMetadata, updateProjectExecutable}: VenvInterface
     }
 
     useEffect(() => {
-      getPackages().then(
-        () => getPackagesToUpdate()
-      )
+      if(!projectMetadata.advanced_config?.user_handled_venv){
+        getPackages().then(
+          () => getPackagesToUpdate()
+        )
+      }
+      
       
     }, [projectMetadata.interpreter])
 
-    if(projectMetadata.interpreter === null){
+    if(projectMetadata.advanced_config?.user_handled_venv){
+      if(projectMetadata.interpreter === null){
+        return <div>
+          <p className="display-6">No interpreter!</p>
+          <p>This project has no interpreter. You can't run a project without an interpreter.</p>
+
+          <button className="btn btn-primary" onClick={() => openDialog().then(
+              (path) => updateProjectExecutable(path)
+          ).catch()}>
+
+          Pick a python executable <i className="bi bi-pencil-fill"></i>
+          </button>
+        </div>
+      }
+      else{
+        return <div className="d-flex">
+          <p>Python executable: {projectMetadata.interpreter}</p>
+          <button className="btn btn-primary ms-2" onClick={() => openDialog().then(
+              (path) => updateProjectExecutable(path)
+          ).catch()}><i className="bi bi-pencil-fill"></i>
+          </button>
+        </div>
+      }
+    }
+
+    else{
+      if(projectMetadata.interpreter === null){
         return <div className="mb-2 rounded">
             <div>
                 <VenvCreateInterface getPythonDefaults={getPythonDefaultExecutables} pythonDefaults={pythonDefaults} isOpen={isCreateModalOpen} handleClose={handleCreateClose} formSubmit={createVenvModal}></VenvCreateInterface>
@@ -82,23 +112,14 @@ function VenvInterface({projectMetadata, updateProjectExecutable}: VenvInterface
             <div>
                 <div>
                 <p className="display-6">No interpreter!</p>
-                <p>This project has no interpreter. You can't run a project without an interpreter. </p>
+                <p>This project has no interpreter. You can't run a project without an interpreter.</p>
 
-                <p>You can choose to pick an arbitrary python executable or let the application handle the python end.</p>
+                <p>Please select an interpreter for the application to use.</p>
 
-                <div className="btn-group">
-                    <button className="btn btn-primary" onClick={() => openDialog().then(
-                        (path) => updateProjectExecutable(path)
-                    ).catch()}>
-
-                    Pick a python executable <i className="bi bi-pencil-fill"></i>
-                    </button>
                     <button className="btn btn-primary" onClick={handleCreateShow}>
-
-                        Let the application handle my python
+                        Choose an interpreter
                     </button>
-                    </div>
-                </div>
+                  </div>
             </div>
         </div>
     }
@@ -127,6 +148,9 @@ function VenvInterface({projectMetadata, updateProjectExecutable}: VenvInterface
             </div>
         </div>
     )
+    }
+
+    
 }
 
 export default VenvInterface
